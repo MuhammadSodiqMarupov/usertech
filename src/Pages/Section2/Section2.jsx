c151b1321c34e77e159e0df78e0353cfbac404dd
MuhammadYusuf <marupovmuhammadyusuf988@gmail.com>
2023-05-06 20:26:02 +0500

    test

---
+++ src/Pages/Section2/Section2.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Section2.scss"
+function Section2(props) {
+    return (
+        <div className="section2">
+            <div className="container">
+                <div className="top">
+                    <div className="left">
+                        <h2> Чем мы можем быть Вам полезны? </h2>
+                        <h1> 95%  </h1>
+                        <h3>наших клиентов окупают услуги уже в первый месяц работы</h3>
+                    </div>
+                    <div className="right">
+                        <p> Разрабатываем веб и мобильные приложения, проводим эффективные маркетинговые кампании.  Как разобраться во всем многообразии услуг  и понять какой именно набор нужен именно Вам? Давайте начнем с разработки концепции! </p>
+                        <h1> 5% </h1>
+                        <h3> клиенты, которые заказывали услуги  и не внедряли наши рекомендации </h3>
+                    </div>
+                </div>
+                <div className="bottom">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Section2;
